51999eeecb079439af03dee7586a92de3619f3b7
ptwohig <patrick@namazustudios.com>
2025-07-02 12:03:40 -0700

    Updated docker compose for deployments.

--- stop-demon.bash
+++ stop-demon.bash
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 set -eux
 
-# Define your Compose file(s)
-COMPOSE_FILE="docker-compose.deployment.yaml"
+# Determine ELEMENTS_UID and ELEMENTS_GID if not already set
+: "${ELEMENTS_UID:=$(id -u)}"
+: "${ELEMENTS_GID:=$(id -g)}"
+
+export ELEMENTS_UID
+export ELEMENTS_GID
+
+# Determine ELEMENTS_DOCKER_COMPOSE_FILE if not already set
+: "${ELEMENTS_DOCKER_COMPOSE_FILE:=docker-compose.deployment.yaml}"
 
 docker compose -f "${COMPOSE_FILE}" down --remove-orphans
